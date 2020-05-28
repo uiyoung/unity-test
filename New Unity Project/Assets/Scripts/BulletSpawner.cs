975520b573f89ccef79db25a1ce37b3f04ecfe2e
uiyoung <suy2suy@gmail.com>
2020-05-28 15:50:31 +0900

    UI in progress

---
+++ New Unity Project/Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    private Transform target;
+    private float minSpawnRate = 0.5f, maxSpawnRate = 3f;
+    private float spawnRate;
+    private float timeAfterSpawn = 0f;
+
+    void Start()
+    {
+        spawnRate = Random.Range(minSpawnRate, maxSpawnRate);
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+
+        if (timeAfterSpawn >= spawnRate)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+
+            timeAfterSpawn = 0;
+            spawnRate = Random.Range(minSpawnRate, maxSpawnRate);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Rotater.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    private float rotateSpeed = 60f;
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+    }
+}
