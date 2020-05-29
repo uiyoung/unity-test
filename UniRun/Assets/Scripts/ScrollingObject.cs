59882d253a03a40c8e953fa6dc812d49b81ce1c6
uiyoung <suy2suy@gmail.com>
2020-05-29 16:42:00 +0900

    background scroll

---
+++ UniRun/Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    private float speed = 10f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        
+    }
+}
