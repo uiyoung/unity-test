b26979a326566e087cd22618d377cfb2d640220d
uiyoung <suy2suy@gmail.com>
2020-05-31 19:49:28 +0900

    sokoban

---
+++ Sokoban/Assets/Scripts/ItemBox.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    public Color changeColor;
+    public bool isArrived = false;
+
+    private Renderer renderer;
+    private Color originalColor;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        originalColor = renderer.material.color;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag =="destination")
+        {
+            renderer.material.color = changeColor;
+            isArrived = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "destination")
+        {
+            renderer.material.color = originalColor;
+            isArrived = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "destination")
+        {
+            renderer.material.color = changeColor;
+            isArrived = true;
+        }
+    }
+
+}
---
+++ Sokoban/Assets/Scripts/Rotater.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(60,60,60)* Time.deltaTime);
+    }
+}
