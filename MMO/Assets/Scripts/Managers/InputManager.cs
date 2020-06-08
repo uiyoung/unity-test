d199262930b03e214f4d55b02eb924e7b2a2011b
uiyoung <suy2suy@gmail.com>
2020-06-08 17:03:28 +0900

    first commit

---
+++ MMO/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager 
+{
+    public Action keyAction = null;
+
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ MMO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float _speed = 10f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (h != 0 || v != 0)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(h, 0, v));
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.2f);
+
+            transform.position += new Vector3(h, 0, v).normalized * _speed * Time.deltaTime;
+        }
+    }
+    
+    void OnKeyboard()
+    {
+
+    }
+}
