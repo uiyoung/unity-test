92634f7c32fbda25b688de5d98cd6c6e68ec48f8
uiyoung <suy2suy@gmail.com>
2020-06-11 16:57:01 +0900

    interface in progress

--- Zombie/Assets/Scripts/GameManager.cs
+++ Zombie/Assets/Scripts/GameManager.cs
@@ -1,59 +1,56 @@
 ﻿using UnityEngine;
 
 // 점수와 게임 오버 여부를 관리하는 게임 매니저
-public class GameManager : MonoBehaviour {
-    // 싱글톤 접근용 프로퍼티
-    public static GameManager instance
+public class GameManager : MonoBehaviour
+{
+    private static GameManager s_instance; // 싱글톤이 할당될 static 변수
+    public static GameManager Instance { get { Init(); return s_instance; } }   // 싱글톤 접근용 프로퍼티
+    public bool IsGameover { get; private set; } // 게임 오버 상태
+
+    private int _score = 0; // 현재 게임 점수
+
+    private void Awake()
     {
-        get
-        {
-            // 만약 싱글톤 변수에 아직 오브젝트가 할당되지 않았다면
-            if (m_instance == null)
-            {
-                // 씬에서 GameManager 오브젝트를 찾아 할당
-                m_instance = FindObjectOfType<GameManager>();
-            }
-
-            // 싱글톤 오브젝트를 반환
-            return m_instance;
-        }
-    }
-
-    private static GameManager m_instance; // 싱글톤이 할당될 static 변수
-
-    private int score = 0; // 현재 게임 점수
-    public bool isGameover { get; private set; } // 게임 오버 상태
-
-    private void Awake() {
         // 씬에 싱글톤 오브젝트가 된 다른 GameManager 오브젝트가 있다면
-        if (instance != this)
+        if (Instance != this)
         {
             // 자신을 파괴
             Destroy(gameObject);
         }
     }
 
-    private void Start() {
+    private void Start()
+    {
         // 플레이어 캐릭터의 사망 이벤트 발생시 게임 오버
         FindObjectOfType<PlayerHealth>().onDeath += EndGame;
     }
 
+    private static void Init()
+    {
+        // 만약 싱글톤 변수에 아직 오브젝트가 할당되지 않았다면
+        // 씬에서 GameManager 오브젝트를 찾아 할당
+        if (s_instance == null)
+            s_instance = FindObjectOfType<GameManager>();
+    }
+
     // 점수를 추가하고 UI 갱신
-    public void AddScore(int newScore) {
+    public void AddScore(int newScore)
+    {
         // 게임 오버가 아닌 상태에서만 점수 증가 가능
-        if (!isGameover)
+        if (!IsGameover)
         {
             // 점수 추가
-            score += newScore;
+            _score += newScore;
             // 점수 UI 텍스트 갱신
-            UIManager.instance.UpdateScoreText(score);
+            UIManager.instance.UpdateScoreText(_score);
         }
     }
 
     // 게임 오버 처리
-    public void EndGame() {
+    public void EndGame()
+    {
         // 게임 오버 상태를 참으로 변경
-        isGameover = true;
+        IsGameover = true;
         // 게임 오버 UI를 활성화
         UIManager.instance.SetActiveGameoverUI(true);
     }
--- Zombie/Assets/Scripts/PlayerMovement.cs
+++ Zombie/Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,44 @@
 ﻿using UnityEngine;
 
 // 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
-public class PlayerMovement : MonoBehaviour {
-    public float moveSpeed = 5f; // 앞뒤 움직임의 속도
-    public float rotateSpeed = 180f; // 좌우 회전 속도
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 5f; // 앞뒤 움직임의 속도
+    [SerializeField]
+    private float _rotateSpeed = 180f; // 좌우 회전 속도
 
+    private PlayerInput _playerInput; // 플레이어 입력을 알려주는 컴포넌트
+    private Rigidbody _rb; // 플레이어 캐릭터의 리지드바디
+    private Animator _anim; // 플레이어 캐릭터의 애니메이터
 
-    private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
-    private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
-    private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
-
-    private void Start() {
-        // 사용할 컴포넌트들의 참조를 가져오기
+    private void Start()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
-    private void FixedUpdate() {
-        // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
+    private void FixedUpdate()
+    {
+        Rotate();
+        Move();
+
+        _anim.SetFloat("Move", _playerInput.Move);
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
-    private void Move() {
-
+    private void Move()
+    {
+        Vector3 moveDist = _playerInput.Move * transform.forward * _moveSpeed * Time.deltaTime;
+        _rb.MovePosition(_rb.position + moveDist);
     }
 
     // 입력값에 따라 캐릭터를 좌우로 회전
-    private void Rotate() {
-
+    private void Rotate()
+    {
+        float turn = _playerInput.Rotate * _rotateSpeed * Time.deltaTime;
+        _rb.rotation = _rb.rotation * Quaternion.Euler(0, turn, 0);
     }
 }
