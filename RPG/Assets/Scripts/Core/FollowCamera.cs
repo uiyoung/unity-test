40b9e0d8648fd041ea5ad9b0def3f554226df703
uiyoung <suy2suy@gmail.com>
2020-09-14 00:20:50 +0900

    movement and camera follow

---
+++ RPG/Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+   [SerializeField] private Transform target;
+    //float distance;
+
+    void Start()
+    {
+        //distance = Mathf.Abs(target.position.y - transform.position.y);
+    }
+
+    void LateUpdate()
+    {
+
+        //transform.position = target.position + new Vector3(0, distance, 0);
+
+        transform.position = target.position;
+    }
+}
---
+++ RPG/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    private NavMeshAgent _nma;
+    private Animator _anim;
+
+    void Start()
+    {
+        _nma = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            _nma.destination = hit.point;
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        // 1. Get the global velocity from NavMeshAgent
+        Vector3 velocity = _nma.velocity;
+        // 2. Convert this into a local value relative to the character
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        // 3. Set the Animator's blend value to be equal to our desired forward speed(on the Z axis)
+        _anim.SetFloat("forwardSpeed", localVelocity.z);
+    }
+}
