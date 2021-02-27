02451587621ab0bf6550620e7999a4ac39a87764
uiyoung <suy2suy@gmail.com>
2021-02-27 23:44:19 +0900

    add attack, smash pot

--- zelda-like/Assets/Scripts/PlayerMovement.cs
+++ zelda-like/Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
 
 public class PlayerMovement : MonoBehaviour
 {
+
     public float speed;
     private Rigidbody2D _rb;
     private Animator _anim;
     private Vector3 _change;
-    private bool _isMoving;
+    private PlayerState _currentState;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
+
+        _currentState = PlayerState.walk;
+        _anim.SetFloat("moveX", 0f);
+        _anim.SetFloat("moveY", -1f);
     }
 
     void FixedUpdate()
@@ -22,26 +33,38 @@ public class PlayerMovement : MonoBehaviour
         _change.x = Input.GetAxisRaw("Horizontal");
         _change.y = Input.GetAxisRaw("Vertical");
 
+        if (_currentState == PlayerState.walk)
+            UpdateAnimationAndMove();
+        if (Input.GetButton("Attack") && _currentState != PlayerState.attack)
+            StartCoroutine(AttackCoroutine());
+    }
+
+    private void UpdateAnimationAndMove()
+    {
         if (_change != Vector3.zero)
         {
             MoveCharacter();
-            UpdateAnimation();
-            _isMoving = true;
+            _anim.SetFloat("moveX", _change.x);
+            _anim.SetFloat("moveY", _change.y);
+            _anim.SetBool("isMoving", true);
         }
         else
-            _isMoving = false;
-
-        _anim.SetBool("isMoving", _isMoving);
+            _anim.SetBool("isMoving", false);
     }
+
 
     private void MoveCharacter()
     {
         _rb.MovePosition(transform.position + _change.normalized * speed * Time.deltaTime);
     }
 
-    private void UpdateAnimation()
+    private IEnumerator AttackCoroutine()
     {
-        _anim.SetFloat("moveX", _change.x);
-        _anim.SetFloat("moveY", _change.y);
+        _anim.SetBool("isAttacking", true);
+        _currentState = PlayerState.attack;
+        yield return null;
+        _anim.SetBool("isAttacking", false);
+        yield return new WaitForSeconds(0.3f);  // 공격속도
+        _currentState = PlayerState.walk;
     }
 }
---
+++ zelda-like/Assets/Scripts/Pot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    private Animator _anim;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public void Smash()
+    {
+        _anim.SetTrigger("Hit");
+    }
+}
